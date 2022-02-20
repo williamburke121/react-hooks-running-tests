aaab3a243396317f934cbf660be9f477c98e23e1
William Warner Burke <williamburke121@gmail.com>
2022-02-19 23:49:34 -0500

    commit tree 2f584acc2868d2b0ea79788c3c2339adf8205df6

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,6 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  screen.debug(element);
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
